93d6cd2005c867f1f81e6cbf96ec3c470a969a0d
andersonlimahw <anderson.lima@soaexpert.com.br>
2017-02-16 17:25:09 -0200

    using multiple modals , with ember-bootstrap lib

--- app/controllers/jobs.js
+++ app/controllers/jobs.js
@@ -1,10 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isWormholeEnabled: false,
-  actions :{
-      toggleModal() {
-        this.toggleProperty('isWormholeEnabled');         
-      }
-  }
+
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,6 +21,8 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
 
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
+
 
   return app.toTree();
 };
